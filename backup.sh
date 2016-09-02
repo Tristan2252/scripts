1760e6f7bc3f3107abc33b5eb3a8e75c36214275
Tristan2252 <tvigil25@live.com>
2016-09-02 10:22:41 -0600

    backup.sh working

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 DATE=$(date +%a-%b-%d)
 BACKUP_SERVER="Crimson-Node-1"
 BACKUP=/mnt/Backup-Drive/Backups
-BACKUP_DIRS="/etc /file-pool"
+BACKUP_DIRS="/etc $@"
 
 
 
@@ -14,6 +14,8 @@ if [ $HOSTNAME == $BACKUP_SERVER ]; then
         rsync -av $i $BACKUP/Node1/
     done
 else
-    rsync -avzhe ssh $i server@Node1:$BACKUP/$HOSTNAME/$i
+    for i in $BACKUP_DIRS; do
+	    rsync --relative -arvzhe ssh $i server@Node1:$BACKUP/$HOSTNAME/$i
+    done
 fi
 
