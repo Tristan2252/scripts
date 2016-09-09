f6ebba73ed92a3b61cbda37f82eaa1ae44669b61
Tristan2252 <tvigil25@live.com>
2016-09-09 10:38:59 -0600

    added error catch to maintenance.sh

--- maintenance.sh
+++ maintenance.sh
@@ -4,5 +4,10 @@ USER="ubuntu"
 HOSTS="$@"
 
 for i in $HOSTS; do
+
+    # for every host in hosts run update and upgrade command
     ssh $USER@$i "sudo apt-get update && sudo apt-get upgrade -y"
+    if [ $? != 0  ]; then 
+        echo "ERROR: exited with non 0"
+    fi
 done 
