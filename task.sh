3ec36a931d9572ae8f1bc15eab368743b530351e
Tristan2252 <tvigil25@live.com>
2017-10-13 03:21:03 +0000

    finished with ansible

--- task.sh
+++ task.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+# run git pull on repo to make sure it is up 
+# to date and then run ansible to take care 
+# of the rest. NOTE: this file should be ln'ed
+# to cron.daily to run daily...
 
+# make sure repo is up to date
 git pull
+
+# run ansible
+ansible-playbook ansible/manage.yml 
