43501a180da73147aed2383194f98d0e89847129
Tristan2252 <tvigil25@live.com>
2017-10-13 02:26:19 +0000

    have ssh_keys on ansible working

--- CS107/PyFiles/Lab3/rectangle_gio.py
+++
@@ -1,8 +0,0 @@
-def area(height, width):
-    area = height * width
-    print("The area of the rectangle is {}".format(area))
-
-
-def per(height, width):
-    perimeter = (2 * height) + (2 * width)
-    print("The perimeter of the rectangel is {}".format(perimeter))
--- CS107/PyFiles/Lab6/test.py
+++
@@ -1,7 +0,0 @@
-cnt = 0
-for i in range(100):
-    if i % 2 > 0:
-        cnt += i
-        print(i)
-
-print(cnt)
